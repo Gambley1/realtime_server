cbc38739748420c935e1475f379bec6fc80ec5bb
Emil <emilzulufov.commercial@gmail.com>
2023-10-10 00:18:14 +0600

    feat: database connection

--- lib/src/data/providers/database_provider.dart
+++ lib/src/data/providers/database_provider.dart
@@ -2,8 +2,14 @@ import 'package:dart_frog/dart_frog.dart';
 import 'package:stormberry/stormberry.dart';
 
 Middleware databaseProvider() {
-  //TODO(): complete database providre
-  final db = Database();
+  //TODO(): replace to env variabled
+  final db = Database(
+    database: 'database',
+    port: 5432,
+    user: 'fl0user',
+    password: 'SReUD1TP7JEl',
+    host: 'ep-twilight-paper-31204147.ap-southeast-1.aws.neon.fl0.io',
+  );
 
   return provider<Database>((context) => db);
 }
